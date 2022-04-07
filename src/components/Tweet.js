4834c372abb68a160ba5f531c0cb0e3d988f9d98
sunwongkim <face712@naver.com>
2022-04-07 20:54:52 +0900

    realtime update 구현

---
+++ src/components/Tweet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tweet() {
+  return <div></div>;
+}
+
+export default Tweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,16 +3,16 @@ import {
   getFirestore,
   collection,
   addDoc,
-  getDocs,
   onSnapshot,
-  doc,
-  where,
   query,
   orderBy,
 } from "firebase/firestore";
 // getDocs
 // https://firebase.google.com/docs/firestore/quickstart
 // https://firebase.google.com/docs/firestore/query-data/get-data
+// REALTIME DATABASE - querySnapshot
+// https://firebase.google.com/docs/firestore/query-data/listen
+// 노마드코더-dbService = firebase.firestore()
 
 function Home({ userObj }) {
   const db = getFirestore();
@@ -20,94 +20,39 @@ function Home({ userObj }) {
   const [tweets, setTweets] = useState([]);
 
   // READ (일반)
-  const getTweets = async () => {
-    const querySnapshot = await getDocs(collection(db, "tweet"));
-    querySnapshot.forEach((doc) => {
-      const newObj = {
+  // useEffect(async () => {
+  //   const querySnapshot = await getDocs(collection(db, "tweet"));
+  //   querySnapshot.forEach((doc) => {
+  //     const newObj = {
+  //       ...doc.data(),
+  //       id: doc.id,
+  //     };
+  //     setTweets((prev) => [newObj, ...prev]);
+  //     console.log(doc.data());
+  //   });
+  // }, []);
+
+  // READ (REALTIME)
+  useEffect(() => {
+    const q = query(collection(db, "tweet"), orderBy("createdAt"));
+    onSnapshot(q, (querySnapshot) => {
+      const tweetArray = querySnapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
-      };
-      setTweets((prev) => [newObj, ...prev]);
-      console.log(doc.data());
+      }));
+      setTweets(tweetArray);
     });
-  };
-  useEffect(() => {
-    getTweets();
   }, []);
-
-  useEffect(() => {
-    // const unsub = onSnapshot(doc(db, "cities", "SF"), (doc) => {
-    //   const tweetArray = doc.docs.map((doc) => ({
-    //     ...doc.data(),
-    //     id: doc.id,
-    //   }));
-    //   setTweets(tweetArray);
-    // });
-    // 노마드코더
-    // dbService.collection("tweet").onSnapshot((snapshot) => {
-    //   const tweetArray = snapshot.docs.map((doc) => ({
-    //     ...doc.data(),
-    //     id: doc.id,
-    //   }));
-    //   setTweets(tweetArray);
-    // });
-  }, []);
-  // 공식문서
-  // const q = query(collection(db, "tweet"), where("state", "==", "CA"));
-  // const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  //   const cities = [];
-  //   querySnapshot.forEach((doc) => {
-  //     cities.push(doc.data().name);
-  //   });
-  //   console.log("Current cities in CA: ", cities.join(", "));
-  // });
-  // unsubscribe();
-  // 유튜브버전
-  // const colRef = collection(db, "tweet");
-  // onSnapshot(colRef, (snapshot) => {
-  //   let books = [];
-  //   snapshot.docs.forEach((doc) => {
-  //     books.push({ ...doc.data(), id: doc.id });
-  //   });
-  //   console.log(books);
-  // });
-
-  // 유튜브 개량
-  useEffect(() => {
-    // const colRef = collection(db, "tweet");
-    // onSnapshot(colRef, (snapshot) => {
-    //   const tweetArray = snapshot.docs.forEach((doc) => ({
-    //     ...doc.data(),
-    //     id: doc.id,
-    //   }));
-    //   setTweets(tweetArray);
-    //   // console.log(tweets);
-    //   console.log("something changed");
-    // });
-  }, []);
-
-  // useEffect(() => {
-  //   const q = query(collection(db, "nweet"), orderBy("createdAt", "desc"));
-  //   onSnapshot(q, (snapshot) => {
-  //     const nweetArr = snapshot.docs.map((doc) => ({
-  //       id: doc.id,
-  //       ...doc.data(),
-  //     }));
-  //     setTweets(nweetArr);
-  //     console.log(tweets);
-  //   });
-  // }, []);
-
   // 노마드코더
-  // useEffect(() => {
-  // dbService.collection("tweet").onSnapshot((snapshot) => {
-  //   const tweetArray = snapshot.docs.map((doc) => ({
-  //     ...doc.data(),
-  //     id: doc.id,
-  //   }));
-  //   setTweets(tweetArray);
+  // useEffect(()=>{
+  //   dbService.collection("tweet").onSnapshot((snapshot) => {
+  //     const tweetArray = snapshot.docs.map((doc) => ({
+  //       ...doc.data(),
+  //       id: doc.id,
+  //     }));
+  //     setTweets(tweetArray);
   //   });
-  // }, []);
+  // },[])
 
   console.log(tweets);
 
@@ -115,15 +60,12 @@ function Home({ userObj }) {
   const onSubmit = async (event) => {
     event.preventDefault();
     const docRef = await addDoc(collection(db, "tweet"), {
-      // tweet,
       text: tweet,
       createdAt: Date.now(),
       creatorId: userObj.uid,
     });
     console.log("Document writtfen with ID: ", docRef.id);
     setTweet("");
-    // 노마드코더-dbService = firebase.firestore()
-    // await dbService.collection("tweets").add({tweet,createdAt: Date.now(),})
   };
   const onChange = (event) => {
     setTweet(event.target.value);
@@ -143,11 +85,11 @@ function Home({ userObj }) {
       </form>
 
       {/* 목록 */}
-      {/* <div>
+      <div key={tweet.id}>
         {tweets.map((tweets) => (
           <h4 key={tweets.id}>{tweets.text}</h4>
         ))}
-      </div> */}
+      </div>
     </>
   );
 }
